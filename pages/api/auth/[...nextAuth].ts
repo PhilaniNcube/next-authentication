d5111b7a012039b75a24878cdd42aade5791ef24
Philani Ncube <ncbphi001@gmail.com>
2023-02-06 16:03:17 +0200

    neax-authentication

---
+++ pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth/next";
+import FacebookProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
+
+
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    })
+  ]
+})
